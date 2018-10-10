0724bf93b0eb089aa3e108075f4404acc8228adf
nhishon <nhishon@luc.edu>
2018-10-09 19:37:12 -0500

    test draw pass

--- app/src/main/java/edu/luc/etl/cs313/android/shapes/android/Draw.java
+++ app/src/main/java/edu/luc/etl/cs313/android/shapes/android/Draw.java
@@ -17,37 +17,60 @@ public class Draw implements Visitor<Void> {
 	private final Paint paint;
 
 	public Draw(final Canvas canvas, final Paint paint) {
-		this.canvas = null; // FIXME
-		this.paint = null; // FIXME
-		paint.setStyle(Style.STROKE);
+
+		this.canvas = canvas; // FIXME
+		this.paint = paint; // FIXME
+
+
+//		paint.setStyle(Style.STROKE);
 	}
 
 	@Override
 	public Void onCircle(final Circle c) {
 		canvas.drawCircle(0, 0, c.getRadius(), paint);
+		;
+
 		return null;
 	}
 
 	@Override
 	public Void onStroke(final Stroke c) {
+		paint.setStyle(Style.STROKE);
+		paint.setColor(c.getColor());
+
+		c.getShape().accept(this);
+		paint.setStyle(Style.STROKE);
+		paint.setColor(c.getColor());
+
 
 		return null;
 	}
 
 	@Override
 	public Void onFill(final Fill f) {
+		paint.setStyle(Style.FILL_AND_STROKE);
+		f.getShape().accept(this);
+
 
 		return null;
 	}
 
 	@Override
 	public Void onGroup(final Group g) {
-
+		for (Shape s : g.getShapes()) {
+			s.accept(this);
+		}
 		return null;
 	}
 
 	@Override
 	public Void onLocation(final Location l) {
+
+		canvas.translate(l.getX(), l.getY());
+
+		l.getShape().accept(this);
+
+		canvas.translate(-l.getX(), -l.getY());
 
 		return null;
 	}
@@ -55,11 +78,19 @@ public class Draw implements Visitor<Void> {
 	@Override
 	public Void onRectangle(final Rectangle r) {
 
+		Draw draw = new Draw(canvas, paint);
+		canvas.drawRect(0, 0, r.getWidth(), r.getHeight(), paint);
+
 		return null;
 	}
 
 	@Override
 	public Void onOutline(Outline o) {
+
+		paint.setStyle(Style.STROKE);
+		o.getShape().accept(this);
+
+
 
 		return null;
 	}
@@ -70,6 +101,7 @@ public class Draw implements Visitor<Void> {
 		final float[] pts = null;
 
 		canvas.drawLines(pts, paint);
+
 		return null;
 	}
 }
