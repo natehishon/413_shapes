ec7ec95174e7a3a711734e351200d3355aa83a9e
Mykola Demianchuk <nicksetone@gmail.com>
2018-10-15 12:41:33 -0500

    Switch to complexGroup

--- app/src/main/java/edu/luc/etl/cs313/android/shapes/android/DrawWidget.java
+++ app/src/main/java/edu/luc/etl/cs313/android/shapes/android/DrawWidget.java
@@ -1,6 +1,5 @@
 package edu.luc.etl.cs313.android.shapes.android;
 
-import edu.luc.etl.cs313.android.shapes.model.*;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Canvas;
@@ -8,37 +7,41 @@ import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
+import edu.luc.etl.cs313.android.shapes.model.BoundingBox;
+import edu.luc.etl.cs313.android.shapes.model.Fixtures;
+import edu.luc.etl.cs313.android.shapes.model.Location;
+import edu.luc.etl.cs313.android.shapes.model.Shape;
+
 public class DrawWidget extends View {
 
-	private final Paint paint = new Paint();
+    private final Paint paint = new Paint();
 
-	public DrawWidget(final Context context, final AttributeSet attrs, final int defStyle) {
-		this(context);
-	}
+    public DrawWidget(final Context context, final AttributeSet attrs, final int defStyle) {
+        this(context);
+    }
 
-	public DrawWidget(final Context context, final AttributeSet attrs) {
-		this(context);
-	}
+    public DrawWidget(final Context context, final AttributeSet attrs) {
+        this(context);
+    }
 
-	public DrawWidget(final Context context) { super(context); }
+    public DrawWidget(final Context context) {
+        super(context);
+    }
 
-	@Override
-	protected void onMeasure(final int widthMeasureSpec, final int heightMeasureSpec) {
-		setMeasuredDimension(widthMeasureSpec, heightMeasureSpec);
-	}
-	
-	// TODO once BoundingBox and Draw are implemented, change Fixtures.simpleCircle
-	// to Fixtures.complexGroup and test the app on an emulator or Android device
-	// to make sure the correct figure is drawn (see Project 3 description for link)
+    @Override
+    protected void onMeasure(final int widthMeasureSpec, final int heightMeasureSpec) {
+        setMeasuredDimension(widthMeasureSpec, heightMeasureSpec);
+    }
 
-	@Override
-	@SuppressLint("DrawAllocation")
-	protected void onDraw(final Canvas canvas) {
-		final Shape shape = Fixtures.simpleCircle;
-		final Location b = shape.accept(new BoundingBox());
-		canvas.translate(-b.getX(), -b.getY());
-		b.accept(new Draw(canvas, paint));
-		shape.accept(new Draw(canvas, paint));
-		canvas.translate(b.getX(), b.getY());
-	}
+
+    @Override
+    @SuppressLint("DrawAllocation")
+    protected void onDraw(final Canvas canvas) {
+        final Shape shape = Fixtures.complexGroup;
+        final Location b = shape.accept(new BoundingBox());
+        canvas.translate(-b.getX(), -b.getY());
+        b.accept(new Draw(canvas, paint));
+        shape.accept(new Draw(canvas, paint));
+        canvas.translate(b.getX(), b.getY());
+    }
 }
