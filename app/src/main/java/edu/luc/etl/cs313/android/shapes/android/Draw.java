fe282a4b9a269a9d90d8c1cf81e7411aa7b28894
Mykola Demianchuk <nicksetone@gmail.com>
2018-10-15 12:42:10 -0500

    Fix onX methods

--- app/src/main/java/edu/luc/etl/cs313/android/shapes/android/Draw.java
+++ app/src/main/java/edu/luc/etl/cs313/android/shapes/android/Draw.java
@@ -3,105 +3,116 @@ package edu.luc.etl.cs313.android.shapes.android;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Paint.Style;
-import edu.luc.etl.cs313.android.shapes.model.*;
+
+import java.util.List;
+
+import edu.luc.etl.cs313.android.shapes.model.Circle;
+import edu.luc.etl.cs313.android.shapes.model.Fill;
+import edu.luc.etl.cs313.android.shapes.model.Group;
+import edu.luc.etl.cs313.android.shapes.model.Location;
+import edu.luc.etl.cs313.android.shapes.model.Outline;
+import edu.luc.etl.cs313.android.shapes.model.Point;
+import edu.luc.etl.cs313.android.shapes.model.Polygon;
+import edu.luc.etl.cs313.android.shapes.model.Rectangle;
+import edu.luc.etl.cs313.android.shapes.model.Shape;
+import edu.luc.etl.cs313.android.shapes.model.Stroke;
+import edu.luc.etl.cs313.android.shapes.model.Visitor;
 
 /**
  * A Visitor for drawing a shape to an Android canvas.
  */
 public class Draw implements Visitor<Void> {
 
-	// TODO entirely your job (except onCircle)
+    private final Canvas canvas;
+    private final Paint paint;
 
-	private final Canvas canvas;
+    public Draw(final Canvas canvas, final Paint paint) {
+        this.canvas = canvas;
+        this.paint = paint;
+        paint.setStyle(Style.STROKE);
+    }
 
-	private final Paint paint;
+    @Override
+    public Void onCircle(final Circle c) {
+        canvas.drawCircle(0, 0, c.getRadius(), paint);
+        return null;
+    }
 
-	public Draw(final Canvas canvas, final Paint paint) {
+    @Override
+    public Void onStroke(final Stroke c) {
+        int color = paint.getColor();
+        paint.setColor(c.getColor());
+        c.getShape().accept(this);
+        paint.setColor(color);
+        return null;
+    }
 
-		this.canvas = canvas; // FIXME
-		this.paint = paint; // FIXME
+    @Override
+    public Void onFill(final Fill f) {
+        Style style = paint.getStyle();
+        paint.setStyle(Style.FILL_AND_STROKE);
+        f.getShape().accept(this);
+        paint.setStyle(style);
+        return null;
+    }
 
+    @Override
+    public Void onGroup(final Group g) {
+        for (Shape s : g.getShapes()) {
+            s.accept(this);
+        }
+        return null;
+    }
 
-//		paint.setStyle(Style.STROKE);
-	}
+    @Override
+    public Void onLocation(final Location l) {
+        canvas.translate(l.getX(), l.getY());
+        l.getShape().accept(this);
+        canvas.translate(-l.getX(), -l.getY());
+        return null;
+    }
 
-	@Override
-	public Void onCircle(final Circle c) {
-		canvas.drawCircle(0, 0, c.getRadius(), paint);
-		;
+    @Override
+    public Void onRectangle(final Rectangle r) {
+        canvas.drawRect(0, 0, r.getWidth(), r.getHeight(), paint);
+        return null;
+    }
 
-		return null;
-	}
+    @Override
+    public Void onOutline(Outline o) {
+        Style style = paint.getStyle();
+        paint.setStyle(Style.STROKE);
+        o.getShape().accept(this);
+        paint.setStyle(style);
+        return null;
+    }
 
-	@Override
-	public Void onStroke(final Stroke c) {
-		paint.setStyle(Style.STROKE);
-		paint.setColor(c.getColor());
+    @Override
+    public Void onPolygon(final Polygon s) {
+        int size = s.getPoints().size();
+        float[] points = new float[size * 4];
 
-		c.getShape().accept(this);
-		paint.setStyle(Style.STROKE);
-		paint.setColor(c.getColor());
+        // Getting X and Y for the first point
+        float startX = s.getPoints().get(0).getX();
+        float startY = s.getPoints().get(0).getY();
+        points[0] = startX;
+        points[1] = startY;
 
+        // Skipping the first point
+        List<? extends Point> remainingPoints = s.getPoints().subList(1, size);
+        int i = 2;
+        for(Point p : remainingPoints) {
+            points[i++] = p.getX();
+            points[i++] = p.getY();
+            points[i++] = p.getX();
+            points[i++] = p.getY();
+        }
 
-		return null;
-	}
+        // Last X and Y must match first point's X and Y
+        points[points.length - 2] = startX;
+        points[points.length - 1] = startY;
 
-	@Override
-	public Void onFill(final Fill f) {
-		paint.setStyle(Style.FILL_AND_STROKE);
-		f.getShape().accept(this);
-
-
-		return null;
-	}
-
-	@Override
-	public Void onGroup(final Group g) {
-		for (Shape s : g.getShapes()) {
-			s.accept(this);
-		}
-		return null;
-	}
-
-	@Override
-	public Void onLocation(final Location l) {
-
-		canvas.translate(l.getX(), l.getY());
-
-		l.getShape().accept(this);
-
-		canvas.translate(-l.getX(), -l.getY());
-
-		return null;
-	}
-
-	@Override
-	public Void onRectangle(final Rectangle r) {
-
-		Draw draw = new Draw(canvas, paint);
-		canvas.drawRect(0, 0, r.getWidth(), r.getHeight(), paint);
-
-		return null;
-	}
-
-	@Override
-	public Void onOutline(Outline o) {
-
-		paint.setStyle(Style.STROKE);
-		o.getShape().accept(this);
-
-
-
-		return null;
-	}
-
-	@Override
-	public Void onPolygon(final Polygon s) {
-
-		final float[] pts = null;
-
-		canvas.drawLines(pts, paint);
-
-		return null;
-	}
+        canvas.drawLines(points, paint);
+        return null;
+    }
 }
